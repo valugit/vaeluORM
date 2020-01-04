05fbd5295615d38c586b86f6bbef816ce61932d9
Aelyce <alicebo.1717@gmail.com>
2020-01-04 15:23:45 +0100

    getEntity works

--- src/BaseEntity.php
+++ src/BaseEntity.php
@@ -5,33 +5,38 @@ namespace VaeluORM;
 class BaseEntity
 {
     private $connection;
+    private $data;
 
     public function __construct($connection)
     {
         $this->connection = $connection;
     }
 
-    public function query($query)
-    {
-        if (!is_string($query) || empty($query)) {
-            throw new Exception("Query not valid");
-        }
+    // public function query($query)
+    // {
+    //     if (!is_string($query) || empty($query)) {
+    //         throw new Exception("Query not valid");
+    //     }
 
-        try {
-            $statement = $this->connection->prepare($query);
-        } catch(PDOException $e) {
-            echo "it doesn't work !";
-            return;
-        }
+    //     try {
+    //         $statement = $this->connection->prepare($query);
+    //     } catch(PDOException $e) {
+    //         echo "it doesn't work !";
+    //         return;
+    //     }
 
-        $statement->execute();
+    //     $statement->execute();
 
-        if ($error[0] != 0) {
-            return false;
-        } else {
-            $result = $statement->fetchAll(PDO::FETCH_OBJ);
-            return $result;
-        }
+    //     if ($error[0] != 0) {
+    //         return false;
+    //     } else {
+    //         $result = $statement->fetchAll(PDO::FETCH_OBJ);
+    //         return $result;
+    //     }
+    // }
+
+    public function fillData($data) {
+        $this->data = $data;
     }
 
     public function getTableName()
